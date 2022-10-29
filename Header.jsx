8e2d468a0bf166b30fa1cd847b6ebdfcc4bb4b9c
Sultan <107674919+Sultan-suan@users.noreply.github.com>
2022-10-30 03:05:29 +0600

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className='header'>
+                <img
+                    src='https://upload.wikimedia.org/wikipedia/ru/thumb/9/9f/Subaru_Logo.svg/1200px-Subaru_Logo.svg.png'
+                    alt='1200px-Subaru_Logo.svg.png'/>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://img3.goodfon.ru/wallpaper/nbig/c/e2/subaru-impreza-wrx-sti-hetchbek.jpg"
+                     alt='subaru-impreza-wrx-sti-hetchbek.jpg'/>
+            </div>
+
+            <div>
+                avatar+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
